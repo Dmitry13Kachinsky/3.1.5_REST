006aaa2592a1614716335d1decdef677d1bbb756
Dmitry <dmstk13@gmail.com>
2023-08-03 20:09:38 +0300

    Commit

--- src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entities/Role.java
@@ -2,21 +2,22 @@ package ru.kata.spring.boot_security.demo.entities;
 
 
 import org.springframework.security.core.GrantedAuthority;
-
 import javax.persistence.*;
-import java.util.Set;
+import java.util.Collection;
+
 
 @Entity
 @Table(name = "sec_roles")
 public class Role implements GrantedAuthority {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private Integer id;
     @Column(name="name")
     private String name;
     @Transient
     @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
+    private Collection<User> users;
 
     public Role() {
     }
@@ -41,11 +42,11 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
-    public Set<User> getUsers() {
+    public Collection<User> getUsers() {
         return users;
     }
 
-    public void setUsers(Set<User> users) {
+    public void setUsers(Collection<User> users) {
         this.users = users;
     }
 

4939b39ce4dd9841b6e5dad746900fad54110464
Dmitry <dmstk13@gmail.com>
2023-08-16 14:07:39 +0300

    Commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,54 +1,24 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.entities.User;
-import ru.kata.spring.boot_security.demo.services.RoleService;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.services.UserService;
-
 
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/")
 public class AdminController {
 
     private final UserService userService;
-    private final RoleService roleService;
 
-    @Autowired
-    public AdminController(UserService userService, RoleService roleService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
     }
 
-    @GetMapping()
+    @GetMapping("/page")
     public String printUsers(ModelMap model, Principal principal) {
         model.addAttribute("userAuth", userService.findByUsername(principal.getName()));
-        model.addAttribute("users", userService.getAllUsers());
-        model.addAttribute("roles", roleService.getAllRoles());
-        model.addAttribute("newUser", new User());
         return "index";
-    }
-
-
-    @PostMapping()
-    public String saveUser(@ModelAttribute("user") User user) {
-        userService.addUser(user);
-        return "redirect:/";
-    }
-
-    @PatchMapping("/user/{id}")
-    public String update(@ModelAttribute("user") User user){
-        userService.updateUser(user);
-        return "redirect:/";
-    }
-
-    @DeleteMapping(value = "/user/{id}")
-    public String removeUser(@PathVariable("id") int id) {
-        userService.removeUser(id);
-        return "redirect:/";
     }
 }
